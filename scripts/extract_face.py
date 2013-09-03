ab9f3f10c85cc974ce04a95a6d0910dd817d4f94
Viblio <viblio@viblio-VB.(none)>
2013-09-03 15:44:45 -0700

    Minor changes to scripts/extract.py

--- scripts/extract_face.py
+++ scripts/extract_face.py
@@ -11,18 +11,20 @@ work_dir = '/tmp/faces-' + str( os.getpid() )
 
 os.makedirs( work_dir )
 
-cmd = '/viblio/bin/viblio_video_analyzer -f %s --analyzers FaceAnalysis --face_detector_cascade_file /viblio/bin/haarcascade_frontalface_default.xml --face_thumbnail_path %s' % ( video_file, work_dir )
+cmd = '/home/viblio/bin/viblio_video_analyzer -f %s --analyzers FaceAnalysis --face_detector_cascade_file /home/viblio/bin/haarcascade_frontalface_alt.xml --face_thumbnail_path %s' % ( video_file, work_dir )
 
 os.system( cmd )
 
 faces = os.listdir( work_dir )
+if len(faces)!=0:
+    sub=os.listdir(work_dir+"/"+faces[0])
 
 if len( faces ) == 0:
+        shutil.rmtree( work_dir )
+        sys.exit( "No faces detected in this video: " + video_file )
+else:
+    shutil.copyfile( work_dir + '/' + faces[0]+'/'+sub[0], face_file )
+    print work_dir + '/' + faces[0]+'/'+sub[0]
     shutil.rmtree( work_dir )
-    sys.exit( "No faces detected in video: " + video_file )
-
-shutil.copyfile( work_dir + '/' + faces[0], face_file )
-
-shutil.rmtree( work_dir )
 
 sys.exit( 0 )
