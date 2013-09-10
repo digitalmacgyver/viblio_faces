f8bca9851fadd578c04d6e0e8f331dd5f897da53
rgolla <rgolla@viblio.com>
2013-09-09 18:41:20 -0700

    Adding thumbnail enhancement to capture more area

--- src/Libraries/Analytics.FaceAnalyzer/Thumbnail.h
+++ src/Libraries/Analytics.FaceAnalyzer/Thumbnail.h
@@ -15,7 +15,7 @@ public:
 	Thumbnail(void);
 	cv::Mat ExtractThumbnail( const cv::Mat &frame,const  cv::Rect &ThumbnailLocation);
 	~Thumbnail(void);
-
+	cv::Rect ConstrainRect(const cv::Rect &rectToConstrain, const cv::Size &imageSize);
 };
 
 	}
